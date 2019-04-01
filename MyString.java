22b351d57b116e99f7b3bd91d72bdd50b0a09a2d
TusharMK <47900477+TusharMK@users.noreply.github.com>
2019-04-02 03:30:10 +0530

    Add files via upload

---
+++ MyString.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class MyString {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "z";     
+        String largest = "a";
+        
+        
+        if(s.length() == k)
+        {
+            return s+"\n"+s;
+        }
+        for(int i = 0 ; i <= s.length()-k ; i++ )
+        {
+            String temp = "";
+            for(int j = i ; j < i+k ; j++ ) 
+            {
+                temp += s.charAt(j);
+            }
+            int x , y ;
+            x = temp.compareTo(smallest);
+            if(x < 0)
+                smallest = temp ;
+          
+            y = temp.compareTo(largest);
+            if(y>0)
+                largest = temp ;
+            
+        }
+        
+        return smallest + "\n" + largest;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
